e4a5ae1315f42819d30b305e974f2d52b3d46a22
Cody-Price <ctprice11@gmail.com>
2019-01-28 15:59:11 -0700

    Inital data entry

---
+++ data.js
@@ -0,0 +1,350 @@
+const vineyards = [
+  {
+    name: 'Addison Farms Vineyard',
+    region: 'Mountains',
+    website: 'www.addisohttp://www.addisonfarms.net/?utm_source=www.ncwine.org&utm_medium=referral&utm_campaign=downstreamnfarms.net',
+    address: '4005 NEW LEICESTER HWY LEICESTER, NC',
+    phoneNumber: '828-581-9463',
+    id: 1
+  },
+  {
+    name: 'Banner Elk Winery',
+    region: 'Mountains',
+    website: 'http://www.bannerelkwinery.com/?utm_source=www.ncwine.org&utm_medium=referral&utm_campaign=downstream',
+    address: '60 Deer Run Lane, Banner Elk, NC 28604',
+    phoneNumber: '828-898-9090',
+    id: 2
+  },
+  {
+    name: 'Belle Nicho Winery',
+    region: 'Mountains',
+    website: 'http://www.bellenichowinery.com/?utm_source=www.ncwine.org&utm_medium=referral&utm_campaign=downstream#wines-section',
+    address: '525 Patton Valley Drive, Nebo, NC 28761',
+    phoneNumber: '828-659-3168',
+    id: 3
+  },
+  {
+    name: 'Biltmore',
+    region: 'Mountains',
+    website: 'https://www.biltmore.com/?utm_source=www.ncwine.org&utm_medium=referral&utm_campaign=downstream',
+    address: 'One Approach Road, Asheville, NC 28803',
+    phoneNumber: '828-225-1333',
+    id: 4
+  },
+  {
+    name: 'Burnsville Wine and...',
+    region: 'Mountains',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Burntshirt Vineyards',
+    region: 'Mountains',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Calaboose Cellars',
+    region: 'Mountains',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Deep Creek Winery',
+    region: 'Mountains',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Eagle Fork Vineyards',
+    region: 'Mountains',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: '',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: 'Addison Farms Vineyard',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  },
+  {
+    name: '30',
+    region: '',
+    website: '',
+    address: '',
+    phoneNumber: '',
+    wines: []
+  }
+]
+
+
+const wines = [
+  {
+    name: 'Gratitude',
+    abv: '',
+    color: 'Red',
+    type: 'Chambourcin',
+    vineyard_id: 1
+  },
+  {
+    name: 'Vitalita Reserve',
+    abv: '',
+    color: 'Red',
+    type: 'Sangiovese',
+    vineyard_id: 1
+  },
+  {
+    name: 'Coming Home',
+    abv: '',
+    color: '',
+    type: 'Cabernet Sauvignon',
+    vineyard_id: 1
+  },
+  {
+    name: 'Cabernet Sauvignon',
+    abv: '',
+    color: 'Red',
+    type: 'Cabernet Sauvignon',
+    vineyard_id: 2
+  },
+  {
+    name: 'Seyval Blanc Reserve (Estate)',
+    abv: '',
+    color: 'White',
+    type: 'Seyval Blanc',
+    vineyard_id: 2
+  },
+  {
+    name: 'Marechal Foch Reserve (Estate)',
+    abv: '',
+    color: 'Red',
+    type: 'Marechal Foch',
+    vineyard_id: 2
+  },
+  {
+    name: 'Chambourcin',
+    abv: '',
+    color: 'Red',
+    type: 'Chambourcin',
+    vineyard_id: 3
+  },
+  {
+    name: 'Rose',
+    abv: '',
+    color: 'Rose',
+    type: 'Cabernet Franc',
+    vineyard_id: 3
+  },
+  {
+    name: 'Strawberry-Rhubarb',
+    abv: '',
+    color: 'Rose',
+    type: 'Strawberry-Rhubarb',
+    vineyard_id: 3
+  },
+  {
+    name: 'Antler Hill Chardonnay Carneros',
+    abv: '',
+    color: 'White',
+    type: 'Chardonnay',
+    vineyard_id: 4
+  },
+  {
+    name: 'Antler Hill Pinot Noir Russian River Valley 2015',
+    abv: '',
+    color: 'Red',
+    type: 'Pinot Noir',
+    vineyard_id: 4
+  },
+  {
+    name: 'Antler Hill Cabernet Sauvignon Napa Valley 2015',
+    abv: '',
+    color: 'Red',
+    type: 'Cabernet Sauvignon',
+    vineyard_id: 4
+  },
+  {
+    name: '',
+    abv: '',
+    color: '',
+    type: '',
+    vineyard_id: ''
+  },
+  {
+    name: '',
+    abv: '',
+    color: '',
+    type: '',
+    vineyard_id: ''
+  },
+  {
+    name: '',
+    abv: '',
+    color: '',
+    type: '',
+    vineyard_id: ''
+  },
+  {
+    name: '',
+    abv: '',
+    color: '',
+    type: '',
+    vineyard_id: ''
+  },
+]
