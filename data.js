e5a1c99d9fbac2a392825456d3cf5d8dbaafa819
Sara Ebbers <johndoe@example.com>
2019-01-28 17:15:08 -0700

    Add Coast Seed Data for Vineyards and wine

--- data.js
+++ data.js
@@ -80,12 +80,84 @@ const vineyards = [
     wines: []
   },
   {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
+    name: 'A Secret Garden Winery',
+    region: 'Coast',
+    website: 'www.asecretgardenwinery.com',
+    address: '1008 Airport Road, Pikeville, NC 27863-9130',
+    phoneNumber: '(919) 734-0260',
+    id: 11
+  },
+  {
+    name: 'Duplin Winery',
+    region: 'Coast',
+    website: 'www.duplinwinery.com',
+    address: '505 N. Sycamore Street, Rose Hill, NC 28458',
+    phoneNumber: '(910) 289-3888',
+   id: 12
+  },
+  {
+    name: 'Huffman Vineyards Winery & Mirey Creek Gifts',
+    region: 'Coast',
+    website: 'www.huffmanvineyards.com',
+    address: '221 Haw Branch Road, Richlands, NC 28574',
+    phoneNumber: '(910) 324-3997',
+    id: 13
+  },
+  {
+    name: 'Lu Mil Vineyard',
+    region: 'Coast',
+    website: 'www.lumilvineyard.com',
+    address: '474 Suggs-Taylor Rd., Dublin, NC 28332',
+    phoneNumber: '(910) 866-5819',
+    id: 14
+  },
+  {
+    name: 'Sanctuary Vineyards',
+    region: 'Coast',
+    website: 'www.sanctuaryvineyards.com',
+    address: '6957 Caratoke Hwy, Jarvisburg, NC 27947',
+    phoneNumber: '(252) 491-2387',
+    id: 15
+  },
+  {
+    name: 'The Country Squire Winery',
+    region: 'Coast',
+    website: 'www.countrysquireinn.com',
+    address: '748 NC 24 & 50, Warsaw, NC 28398',
+    phoneNumber: '(910) 296-1727',
+    id: 16
+  },
+  {
+    name: 'Grapefull Sisters Vineyard',
+    region: 'Coast',
+    website: 'www.grapefullsistersvineyard.com',
+    address: '4903 Ramsey Ford Road, Tabor City, NC 28463',
+    phoneNumber: '(910) 653-2944',
+    id: 17
+  },
+  {
+    name: 'Lake Road Winery',
+    region: 'Coast',
+    website: 'www.lakeroadwinery.com',
+    address: '1120 Lake Road, Newport, NC 28570',
+    phoneNumber: '(252) 622-0930',
+    id: 18
+  },
+  {
+    name: 'Silver Coast Winery',
+    region: 'Coast',
+    website: 'www.silvercoastwinery.com',
+    address: '6680 Barbeque Rd., Ocean Isle Beach, NC 28469',
+    phoneNumber: '(910) 287-2800',
+    id: 19
+  },
+  {
+    name: 'Vineyards on the Scuppernong',
+    region: 'Coast',
+    website: 'www.vineyardsonthescuppernong.com',
+    address: '117 Elm St, Columbia, NC 27925',
+    phoneNumber: '(252) 796-4727',
+    id: 20
   },
   {
     name: 'Addison Farms Vineyard',
@@ -93,79 +165,7 @@ const vineyards = [
     website: '',
     address: '',
     phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
-  },
-  {
-    name: 'Addison Farms Vineyard',
-    region: '',
-    website: '',
-    address: '',
-    phoneNumber: '',
-    wines: []
+    id: 21
   },
   {
     name: 'Addison Farms Vineyard',
@@ -341,10 +341,213 @@ const wines = [
     vineyard_id: ''
   },
   {
+    name: 'Mary Rose Red',
+    abv: '',
+    color: 'Red',
+    type: 'Dry',
+    vineyard_id: '11'
+  },
+  {
+    name: 'Hush-Hush Blush',
+    abv: '',
+    color: 'Blush',
+    type: 'Sweet',
+    vineyard_id: '11'
+  },
+  {
+    name: 'Strawberry Squeeze',
+    abv: '',
+    color: 'Blush',
+    type: 'Semi-dry',
+    vineyard_id: '11'
+  },
+  {
+    name: 'Hatteras Red',
+    abv: '',
+    color: 'Red',
+    type: 'Sweet Muscadine',
+    vineyard_id: '12'
+  },
+  {
+    name: 'Carolina Red',
+    abv: '',
+    color: 'Red',
+    type: 'Sweet',
+    vineyard_id: '12'
+  },
+  {
+    name: 'Costal Shag',
+    abv: '',
+    color: 'Red',
+    type: 'Fruity',
+    vineyard_id: '12'
+  },
+  {
+    name: 'Fire Side Red',
+    abv: '',
+    color: 'Red',
+    type: 'Dry',
+    vineyard_id: '13'
+  },
+  {
+    name: 'Carolina Sunshine',
+    abv: '',
+    color: 'white',
+    type: 'Riesling',
+    vineyard_id: '13'
+  },
+  {
+    name: 'Strawberry',
+    abv: '',
+    color: 'Blush',
+    type: 'Sweet',
+    vineyard_id: '13'
+  },
+  {
+    name: 'Old Cumberland',
+    abv: '',
+    color: 'White',
+    type: 'Muscadine',
+    vineyard_id: '14'
+  },
+  {
+    name: 'Bladen Blush',
+    abv: '',
+    color: 'white',
+    type: 'Muscadine',
+    vineyard_id: '14'
+  },
+  {
+    name: 'Cape Owen Red',
+    abv: '',
+    color: 'Red',
+    type: 'Muscadine',
+    vineyard_id: '14'
+  },
+  {
+    name: 'Wild Pony White',
+    abv: '',
+    color: 'White',
+    type: 'Dry',
+    vineyard_id: '15'
+  },
+  {
+    name: 'Morton',
+    abv: '',
+    color: 'Red',
+    type: 'Blend',
+    vineyard_id: '15'
+  },
+  {
+    name: 'OBX Ice',
+    abv: '',
+    color: 'White',
+    type: 'Dessert',
+    vineyard_id: '15'
+  },
+  {
+    name: 'Pride of Scotland',
+    abv: '',
+    color: 'White',
+    type: 'Chardonnay',
+    vineyard_id: '16'
+  },
+  {
+    name: 'Sunken Garden Thistle',
+    abv: '',
+    color: 'White',
+    type: 'Strawberry',
+    vineyard_id: '16'
+  },
+  {
+    name: 'Highlander',
+    abv: '',
+    color: 'Red',
+    type: 'Cabernet Sauvignon',
+    vineyard_id: '16'
+  },
+  {
+    name: 'Waccamaw White',
+    abv: '',
+    color: 'White',
+    type: 'Fruity',
+    vineyard_id: '17'
+  },
+  {
+    name: 'Sunset Blush',
+    abv: '',
+    color: 'Blush',
+    type: 'Fruity',
+    vineyard_id: '17'
+  },
+  {
+    name: 'Picnic Red',
+    abv: '',
+    color: 'Red',
+    type: 'Cooking',
+    vineyard_id: '17'
+  },
+  {
+    name: 'After Dark',
+    abv: '',
+    color: 'Red',
+    type: 'Sweet',
+    vineyard_id: '18'
+  },
+  {
+    name: 'Doreen',
+    abv: '',
+    color: 'White',
+    type: 'Muscadine',
+    vineyard_id: '18'
+  },
+  {
+    name: 'Strawberry Patch',
+    abv: '',
+    color: 'Pink',
+    type: 'Sweet',
+    vineyard_id: '18'
+  },
+  {
+    name: 'Calabash White',
+    abv: '',
+    color: 'White',
+    type: 'Refreshing',
+    vineyard_id: '19'
+  },
+  {
+    name: 'Bella Rosa Dry Rose',
+    abv: '',
+    color: 'Pink',
+    type: 'Blush',
+    vineyard_id: '19'
+  },
+  {
+    name: 'Touriga (Cape Fear Blood Wine)',
+    abv: '',
+    color: 'Red',
+    type: 'Complex',
+    vineyard_id: '19'
+  },
+  {
+    name: 'Catawba',
+    abv: '',
+    color: 'White',
+    type: 'Semi-sweet',
+    vineyard_id: '20'
+  },
+  {
     name: '',
     abv: '',
-    color: '',
+    color: 'Red',
     type: '',
-    vineyard_id: ''
+    vineyard_id: '20'
+  },
+  {
+    name: 'Somerset (Tangerine)',
+    abv: '',
+    color: 'White',
+    type: 'Sweet',
+    vineyard_id: '20'
   },
 ]
