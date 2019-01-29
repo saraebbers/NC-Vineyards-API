5f9b513361c10130fdfd8b91d79f88f40bf6731a
Sara Ebbers <johndoe@example.com>
2019-01-29 13:27:42 -0700

    Fix configuration spelling error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile.js')[environment];
-const database = require('knex')(congiration)
+const database = require('knex')(configuration)
 
 
 const express = require('express');
