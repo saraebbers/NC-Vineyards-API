67216e5c5047e1f043cbb9bbaf1e642f0386cd43
Cody-Price <ctprice11@gmail.com>
2019-01-31 11:21:14 -0700

    Capitalize PORT in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 
 app.use( bodyParser.json() );
 
-app.set('port', process.env.port || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'NC Vineyards';
 
